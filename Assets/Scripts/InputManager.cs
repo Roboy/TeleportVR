cd1afc961ad6e205894d983bb9d65be1e0e8dcfe
PN <philngbr@googlemail.com>
2021-02-19 20:11:14 +0100

    Added RoboyFace and implemented AnimusSE script

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class InputManager : Singleton<InputManager>
 {
-    List<UnityEngine.XR.InputDevice> controllerLeft = new List<UnityEngine.XR.InputDevice>();
-    List<UnityEngine.XR.InputDevice> controllerRight = new List<UnityEngine.XR.InputDevice>();
+    public List<UnityEngine.XR.InputDevice> controllerLeft = new List<UnityEngine.XR.InputDevice>();
+    public List<UnityEngine.XR.InputDevice> controllerRight = new List<UnityEngine.XR.InputDevice>();
 
     private bool lastMenuBtn;
     private bool lastGrabLeft;
@@ -13,47 +13,85 @@ public class InputManager : MonoBehaviour
     
     void Start()
     {
-        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.Left, controllerLeft);
-        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.Right, controllerRight);
+        GetLeftController();
+        GetRightController();
     }
-    
-    void Update()
-    {
-        bool btn;
-        if (controllerLeft.Count > 0) {
-            if (controllerLeft[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.menuButton, out btn) && btn && !lastMenuBtn)
-            {
-                StateManager.Instance.GoToNextState();
-            }
-            lastMenuBtn = btn;
-                
-            if (controllerLeft[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out btn) && btn && !lastGrabLeft)
-            {
-                Widgets.WidgetInteraction.Instance.ToggleRightBody();
-            }
-            lastGrabLeft = btn;
-        }
-        else {
+
+    /// try to get the left controller, if possible.<!-- return if the controller can be referenced.-->
+    public bool GetLeftController() {
+        if (controllerLeft.Count == 0) {
             UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.Left, controllerLeft);
         }
-        if (controllerRight.Count > 0) {
-            if (controllerRight[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out btn) && btn && !lastGrabRight)
-            {
-                Widgets.WidgetInteraction.Instance.ToggleLeftBody();
-            }
-            lastGrabRight = btn;
-        }
-        else {
+        return controllerLeft.Count > 0;
+    }
+
+    /// try to get the right controller, if possible.<!-- return if the controller can be referenced.-->
+    public bool GetRightController() {
+        if (controllerRight.Count == 0) {
             UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.Right, controllerRight);
         }
+        return controllerRight.Count > 0;
+    }
+    
+    void FixedUpdate()
+    {
+        if (!Widgets.WidgetInteraction.settingsAreActive) {
+            bool btn;
+            if (GetLeftController()) {
+                // Go to the other mode
+                if (controllerLeft[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.menuButton, out btn) && btn && !lastMenuBtn)
+                {
+                    StateManager.Instance.GoToNextState();
+                }
+                lastMenuBtn = btn;
+                    
+                /*if (controllerLeft[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out btn) && btn && !lastGrabLeft)
+                {
+                    Widgets.WidgetInteraction.Instance.ToggleRightBody();
+                }
+                lastGrabLeft = btn;*/
+
+                // update the emotion buttons
+                if (controllerLeft[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out btn)) {
+                    UnityAnimusClient.Instance.LeftButton1 = btn;
+                }
+                if (controllerLeft[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out btn)) {
+                    UnityAnimusClient.Instance.LeftButton2 = btn;
+                }
+            }
+            else {
+                UnityAnimusClient.Instance.LeftButton1 = Input.GetKeyDown(KeyCode.F);
+                UnityAnimusClient.Instance.LeftButton2 = Input.GetKeyDown(KeyCode.R);
+            }
+            if (GetRightController()) {
+                /*if (controllerRight[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.gripButton, out btn) && btn && !lastGrabRight)
+                {
+                    Widgets.WidgetInteraction.Instance.ToggleLeftBody();
+                }
+                lastGrabRight = btn;*/
+                
+                // update the emotion buttons
+                if (controllerRight[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out btn)) {
+                    UnityAnimusClient.Instance.RightButton1 = btn;
+                }
+                if (controllerRight[0].TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out btn)) {
+                    UnityAnimusClient.Instance.RightButton2 = btn;
+                }
+            }
+            else {
+                UnityAnimusClient.Instance.RightButton1 = Input.GetKeyDown(KeyCode.G);
+                UnityAnimusClient.Instance.RightButton2 = Input.GetKeyDown(KeyCode.T);
+            }
+
+        }
         
-        if (Input.GetKeyDown(KeyCode.F))
+        /*if (Input.GetKeyDown(KeyCode.F))
         {
             Widgets.WidgetInteraction.Instance.ToggleRightBody();
         }
         if (Input.GetKeyDown(KeyCode.G))
         {
             Widgets.WidgetInteraction.Instance.ToggleLeftBody();
-        }
+        }*/
     }
 }
