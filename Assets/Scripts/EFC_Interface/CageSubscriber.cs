0d1ca049336d4757c402ac196d1b059e787f01aa
Philip <philngbr@googlemail.com>
2021-03-11 14:07:03 +0100

    Costum ros messages work now

---
+++ Assets/Scripts/EFC_Interface/CageSubscriber.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RosSharp.RosBridgeClient;
+using RosSharp.RosBridgeClient.MessageTypes.RoboyMiddleware;
+using UnityEngine;
+
+public class CageSubscriber : UnitySubscriber<ExoforceResponse>
+{
+
+    protected override void ReceiveMessage(ExoforceResponse message)
+    {
+        Debug.Log(message.success + ": Received " + message.message);
+        if (message.success)
+        {
+            CageInterface.cageIsConnected = true;
+        }
+    }
+}
