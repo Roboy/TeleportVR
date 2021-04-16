13744765e92806343711e3a827439327a478d3ff
Max Fruehauf <max@maxfruehauf.com>
2021-04-16 08:31:12 +0200

    Remove irrelevant changes and make variables const

--- Assets/Roboy/Wheelchair/Scripts/OrientationOffset.cs
+++ Assets/Roboy/Wheelchair/Scripts/OrientationOffset.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class OrientationOffset : MonoBehaviour
 {
-
+    // Controller GameObject to take orientation from
     public Transform controller;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        // Derived by experimentation
-        Quaternion offset = Quaternion.Euler(-189.118f, -8.403992f, 15.2381f);
+    {
+        // Derived by placing an empty GameObject at the writs position and
+        // having the IK goal following it's rotation. 
+        // By then applying rotations manually to this game object to visually align Roboy's
+        // hand with the Oculus controllers we derived the following constants
+        const float rx = -189.118f, ry = -8.403992f, rz = 15.2381f;
+        Quaternion offset = Quaternion.Euler(rx, ry, rz);
         transform.rotation = controller.rotation * offset;
-        // Debug.Log(transform.rotation);
     }
 }
