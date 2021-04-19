acd475ec5df3f5e41d4f49b1345981a4aa8b968f
Max Fruehauf <max@maxfruehauf.com>
2021-04-19 21:00:22 +0200

    Add manual rotation copy of joints

---
+++ Assets/CopyTransfrom.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransfrom : MonoBehaviour
+{
+
+    public Transform controller;
+
+    public bool position = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (controller != null && position)
+        {
+            transform.position = controller.position;
+        }
+        if (controller != null)
+        {
+            transform.rotation= controller.rotation;
+        }
+
+    }
+}
