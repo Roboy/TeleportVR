151eca631cc98191820b2d60f64dd00d401d2774
Max Fruehauf <max@maxfruehauf.com>
2021-04-20 13:51:56 +0200

    Add first working prototype

--- Assets/CopyTransfrom.cs
+++ Assets/CopyTransfrom.cs
@@ -7,12 +7,11 @@ public class CopyTransfrom : MonoBehaviour
 
     public Transform controller;
 
-    public bool position = false; 
-
+    public bool position = false;
+    public bool rotation = true;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -22,9 +21,9 @@ public class CopyTransfrom : MonoBehaviour
         {
             transform.position = controller.position;
         }
-        if (controller != null)
+        if (controller != null && rotation)
         {
-            transform.rotation= controller.rotation;
+            transform.rotation = controller.rotation;
         }
 
     }
