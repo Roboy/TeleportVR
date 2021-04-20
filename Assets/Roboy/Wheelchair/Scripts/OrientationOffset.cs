01627cb798f0a87d4124a80ee89c8c19e504f5c0
Max Fruehauf <max@maxfruehauf.com>
2021-04-20 14:10:25 +0200

    Minor changes to OrientationOffset.cs

--- Assets/Roboy/Wheelchair/Scripts/OrientationOffset.cs
+++ Assets/Roboy/Wheelchair/Scripts/OrientationOffset.cs
@@ -11,6 +11,13 @@ public class OrientationOffset : MonoBehaviour
     // Objective Script controlling the hand orienation
     public BioIK.Orientation orientationObjective;
 
+    // Derived by placing an empty GameObject at the writs position and
+    // having the IK goal following it's rotation. 
+    // By then applying rotations manually to this game object to visually align Roboy's
+    // hand with the Oculus controllers we derived the following constants
+    public float rotationX = -189.118f, rotationY = -8.403992f, rotationZ = 15.2381f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +26,7 @@ public class OrientationOffset : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Derived by placing an empty GameObject at the writs position and
-        // having the IK goal following it's rotation. 
-        // By then applying rotations manually to this game object to visually align Roboy's
-        // hand with the Oculus controllers we derived the following constants
-        const float rx = -189.118f, ry = -8.403992f, rz = 15.2381f;
-        Quaternion offset = Quaternion.Euler(rx, ry, rz);
-
+        Quaternion offset = Quaternion.Euler(rotationX, rotationY, rotationZ);
         if (trackOrientation)
         {
             orientationObjective.Weight = 1;
